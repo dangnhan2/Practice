8c9d3d7e8b576d9293ec8c63cf5f79344974f0f4
Your Name <you@example.com>
2025-03-28 17:30:39 +0700

    first commit

---
+++ LearnGit/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGit
+{
+    public class Teacher : Person
+    {
+        public string Subject { get; set; } = string.Empty;
+        public int YearsOfExperience { get; set; }
+    }
+}
