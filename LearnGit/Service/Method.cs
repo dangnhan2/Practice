a20a2113b32a80229b0aabedd04f2108720beb63
Your Name <you@example.com>
2025-03-28 17:55:42 +0700

    cập nhật lần 2

---
+++ LearnGit/Service/Method.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGit.Service
+{
+    public class Method
+    {
+        List<Employee> employees = new List<Employee>();
+        List<Teacher> teachers = new List<Teacher>();
+    }
+}
